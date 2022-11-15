73b9c4d8bab234070c4134a003a32f75efeeb5cc
Vasya <vasyll.danylenko@gmail.com>
2022-11-15 14:43:23 +0300

    Empty/Null list tests add.

--- src/test/java/org/example/IDateSorterImplTest.java
+++ src/test/java/org/example/IDateSorterImplTest.java
@@ -3,10 +3,12 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IDateSorterImplTest {
 
@@ -15,17 +17,41 @@ class IDateSorterImplTest {
             LocalDate.of(2005, 1, 1), LocalDate.of(2005, 5, 3));
 
     @Test
-    void sortDates_ListOfDate_SortedListOfDate() {
+    void sortDates_listOfDate_SortedListOfDate() {
 
         Collection<LocalDate> actual = List.of(
                 LocalDate.of(2005, 1, 1), LocalDate.of(2005, 1, 2),
                 LocalDate.of(2005, 7, 1), LocalDate.of(2005, 5, 3));
 
         IDateSorterImpl iDateSorter = new IDateSorterImpl();
-
         Collection<LocalDate> expected = iDateSorter.sortDates(localDateList);
 
         assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void sortDates_EmptyList_EmptyList() {
+
+        Collection<LocalDate> actual = new ArrayList<>();
+        IDateSorterImpl iDateSorter = new IDateSorterImpl();
+
+        Collection<LocalDate> expected = iDateSorter.sortDates(List.of());
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void sortDates_nullList_ShouldThrowError() {
+
+        List<LocalDate> list = null;
+        IDateSorterImpl iDateSorter = new IDateSorterImpl();
+
+        NullPointerException nullPointerException = assertThrows(
+                NullPointerException.class, () -> iDateSorter.sortDates(list));
+
+        assertEquals(NullPointerException.class, nullPointerException.getClass());
 
     }
 
