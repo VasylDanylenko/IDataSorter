7ee911fa35c9ea04943d69bec106539ea99837ed
Vasya <vasyll.danylenko@gmail.com>
2022-11-15 14:42:23 +0300

    Refactoring.

--- src/main/java/org/example/IDateSorterImpl.java
+++ src/main/java/org/example/IDateSorterImpl.java
@@ -4,24 +4,26 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IDateSorterImpl implements IDateSorter {
 
-    private final List<Integer> month = List.of(5, 6, 7, 8);
-    private final List<Integer> rMonth = List.of(1, 2, 3, 4, 9, 10, 11, 12);
+    private final List<Integer> rMonths = List.of(1, 2, 3, 4, 9, 10, 11, 12);
 
     @Override
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
 
-        List<LocalDate> rMonthSortedList = unsortedDates.stream()
-                .filter(e -> rMonth.contains(e.getMonthValue())).sorted().toList();
+        Map<Boolean, List<LocalDate>> collect = unsortedDates.stream()
+                .collect(Collectors.partitioningBy(a -> rMonths.contains(a.getMonthValue())));
 
-        List<LocalDate> monthReverseSortedList = unsortedDates.stream()
-                .filter(e -> month.contains(e.getMonthValue())).sorted(Comparator.reverseOrder()).toList();
+        List<LocalDate> withR = collect.get(true);
+        List<LocalDate> withoutR = collect.get(false);
 
-        return Stream.concat(rMonthSortedList.stream(), monthReverseSortedList.stream()).collect(Collectors.toList());
+
+        return Stream.concat(withR.stream().sorted(), withoutR.stream().sorted(Comparator.reverseOrder()))
+                .collect(Collectors.toList());
 
     }
 
